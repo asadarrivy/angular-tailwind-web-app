28fb026abaac377004948c5da2f6c49cc55e0dfb
asadarrivy <asad.arrivy@gmail.com>
2026-04-21 03:26:44 +0500

    my-first-angular-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,94 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-tailwind-app';
+  title = 'Angular + Tailwind Working 🚀';
+
+  item = [
+    {
+      logo: 'Logo 1',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaque.',
+      link: 'learn more',
+    },
+    {
+      logo: 'Logo 2',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaque.',
+      link: 'learn more',
+    },
+    {
+      logo: 'Logo 3',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaque.',
+      link: 'learn more',
+    },
+    {
+      logo: 'Logo 4',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaque.',
+      link: 'learn more',
+    },
+    {
+      logo: 'Logo 5',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaque.',
+      link: 'learn more',
+    },
+    {
+      logo: 'Logo 6',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaque.',
+      link: 'learn more',
+    },
+    {
+      logo: 'Logo 7',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaque.',
+      link: 'learn more',
+    },
+    {
+      logo: 'Logo 8',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaque.',
+      link: 'learn more',
+    },
+    {
+      logo: 'Logo 9',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaque.',
+      link: 'learn more',
+    },
+    {
+      logo: 'Logo 10',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaque.',
+      link: 'learn more',
+    },
+    {
+      logo: 'Logo 11',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaqueLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaque.',
+      link: 'learn more',
+    },
+  ];
+
+  selectedItem: any = null;
+  showModal: boolean = false;
+
+  openModal(item: any) {
+    this.selectedItem = item;
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
 }
